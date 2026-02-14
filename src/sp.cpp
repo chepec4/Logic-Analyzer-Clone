5aaf44809a32dfffa684d8206c4958a1376ece0a
chepec4 <chepec4@gmail.com>
2026-02-13 18:32:25 -0600

    Update sp.cpp

--- src/sp.cpp
+++ src/sp.cpp
@@ -1,19 +1,23 @@
 #include <cstdio>
 #include <cstdlib>
 
+
 #include "kali/platform.h"
 #include "sp/sp.h"
 
 using namespace sp;
 
+
 inline m4f inter(const m4f& x, const m4f& y)
 {
     m4f r;
+    // CORRECCIÓN PROFUNDA:
+    // m4f es un 'sp::array', no una struct con miembro 'v'.
+    // Accedemos directamente a los indices del objeto.
     r[0] = y[0] - x[0];
     r[1] = y[2] - x[1];
     r[2] = y[1] - x[2];
     r[3] = y[2] - x[3];
     return r;
 }
-
 

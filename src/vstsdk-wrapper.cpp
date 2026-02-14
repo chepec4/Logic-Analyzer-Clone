daa4baa23e2c6c27e0df07da9faec32fdb5c34be
chepec4 <chepec4@gmail.com>
2026-02-13 21:06:47 -0600

    Reformat vstsdk-wrapper.cpp for consistency

--- src/vstsdk-wrapper.cpp
+++ src/vstsdk-wrapper.cpp
@@ -1,21 +1,17 @@
-#include "main.h" // Vital que vaya primero
+#include "main.h" 
 #include "includes.h"
 
-// Desactivar warnings de Steinberg
 #pragma warning(push, 3)
 #include "audioeffect.cpp"
 #include "audioeffectx.cpp"
 #pragma warning(pop)
 
-// LA CLAVE DE LA VISIBILIDAD
 extern "C" {
     __declspec(dllexport) AEffect* VSTPluginMain(audioMasterCallback master) {
         Plugin* effect = new Plugin(master);
         if (!effect) return 0;
         return effect->getAeffect();
     }
-
-    // Compatibilidad extra
     __declspec(dllexport) AEffect* main(audioMasterCallback master) {
         return VSTPluginMain(master);
     }

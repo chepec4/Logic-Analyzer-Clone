d47d3c46c9fdc0e2bcd45308f241e2f212040caf
chepec4 <chepec4@gmail.com>
2026-02-13 20:20:17 -0600

    Refactor VSTPluginMain and clean up code
    Removed legacy main function alias and debug definition.

--- src/vstsdk-wrapper.cpp
+++ src/vstsdk-wrapper.cpp
@@ -1,21 +1,21 @@
+#ifndef _DEBUG
+#define _DEBUG 0
+#endif
+
 #include "main.h"
 #include "includes.h"
 
+// Forzamos la inclusión del código fuente del SDK
 #pragma warning(push, 3)
 #include "audioeffect.cpp"
 #include "audioeffectx.cpp"
 #pragma warning(pop)
 
-// Esta función es el estándar que FL Studio busca para cargar el plugin
 extern "C" {
+    // Definimos VSTPluginMain como la única entrada válida
     __declspec(dllexport) AEffect* VSTPluginMain(audioMasterCallback master) {
         Plugin* effect = new Plugin(master);
         if (!effect) return 0;
         return effect->getAeffect();
-    }
-    
-    // Alias por si el DAW es muy antiguo
-    __declspec(dllexport) AEffect* main(audioMasterCallback master) {
-        return VSTPluginMain(master);
     }
 }

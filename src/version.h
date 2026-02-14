d1b86c4315c290d4fefa2be15b5da9b10598b522
chepec4 <chepec4@gmail.com>
2026-02-13 18:42:28 -0600

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,32 +1,16 @@
+#ifndef VERSION_INCLUDED
+#define VERSION_INCLUDED
 
-#define VER       1,0,9
-#define NAME      "Spectrum Analyzer"
-#define COMPANY   "Seven Phases"
-#define COPYRIGHT "© Max Mikhailov, 2009-2015"
+// Definicion manual para evitar errores de macros antiguas
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+#define VERSION_BUILD 0
 
-// ............................................................................
+// Cadena de texto directa
+#define VERSION_STR "1.0.0"
 
-#define PP_STR(A)          PP_STR__(A)
-#define PP_STR_1(A)        PP_STR(A)
-#define PP_STR__(A)        #A
+// Version numerica para recursos de Windows
+#define VERSION_RC 1,0,0,0
 
-#define PP_CAT(A, B)       PP_CAT__(A, B)
-#define PP_CAT__(A, B)     A##B
-
-#define VERSION__(A, B, C) A.PP_CAT(B, C)
-
-#define VERSION            PP_CAT(VERSION__, (VER))
-#define VERSION_STR        PP_STR_1(VERSION)
-#define VERSION_RC         VER, 0
-
-// ............................................................................
-
-#ifdef PREPROCESS_ONLY
-
-VERSION
-VERSION_RC
-VERSION_STR
-
-#endif // ~ PREPROCESS_ONLY
-
-// ............................................................................
+#endif // VERSION_INCLUDED

32ec02269de7f52d1b8934d7223a36d85b0ca3b3
chepec4 <chepec4@gmail.com>
2026-02-13 20:42:56 -0600

    Add legacy alias for VSTPluginMain
    Added legacy alias for VSTPluginMain function.

--- src/vstsdk-wrapper.cpp
+++ src/vstsdk-wrapper.cpp
@@ -1,21 +1,23 @@
-#ifndef _DEBUG
-#define _DEBUG 0
-#endif
-
-#include "main.h"
+// IMPORTANTE: main.h debe ir primero
+#include "main.h" 
 #include "includes.h"
 
-// Forzamos la inclusión del código fuente del SDK
+// Desactivamos warnings molestos del SDK
 #pragma warning(push, 3)
 #include "audioeffect.cpp"
 #include "audioeffectx.cpp"
 #pragma warning(pop)
 
 extern "C" {
-    // Definimos VSTPluginMain como la única entrada válida
+    // Puerta de entrada para el DAW
     __declspec(dllexport) AEffect* VSTPluginMain(audioMasterCallback master) {
         Plugin* effect = new Plugin(master);
         if (!effect) return 0;
         return effect->getAeffect();
+    }
+    
+    // Alias legacy
+    __declspec(dllexport) AEffect* main(audioMasterCallback master) {
+        return VSTPluginMain(master);
     }
 }

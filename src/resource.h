f5d38de5a37b15af27b956a17abb1c0c740829f1
chepec4 <chepec4@gmail.com>
2026-02-13 19:03:50 -0600

    Create resource.h

---
+++ src/resource.h
@@ -0,0 +1,14 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by dialogs.rc
+
+#ifndef RESOURCE_INCLUDED
+#define RESOURCE_INCLUDED
+
+// Definiciones estándar para evitar errores en los diálogos
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           100
+
+#endif // RESOURCE_INCLUDED

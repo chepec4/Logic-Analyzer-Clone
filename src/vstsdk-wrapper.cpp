c8434468e1c626b8dfa60c226cb322876f50c298
chepec4 <chepec4@gmail.com>
2026-02-13 20:49:35 -0600

    Refactor VST plugin entry point structure

--- src/vstsdk-wrapper.cpp
+++ src/vstsdk-wrapper.cpp
@@ -1,23 +1,15 @@
-// IMPORTANTE: main.h debe ir primero
-#include "main.h" 
+#include "main.h" // ¡Siempre primero!
 #include "includes.h"
 
-// Desactivamos warnings molestos del SDK
 #pragma warning(push, 3)
 #include "audioeffect.cpp"
 #include "audioeffectx.cpp"
 #pragma warning(pop)
 
 extern "C" {
-    // Puerta de entrada para el DAW
     __declspec(dllexport) AEffect* VSTPluginMain(audioMasterCallback master) {
         Plugin* effect = new Plugin(master);
         if (!effect) return 0;
         return effect->getAeffect();
-    }
-    
-    // Alias legacy
-    __declspec(dllexport) AEffect* main(audioMasterCallback master) {
-        return VSTPluginMain(master);
     }
 }

4b03dbdd38bc543e3aaac035b6508a32920cbb87
chepec4 <chepec4@gmail.com>
2026-02-13 20:59:08 -0600

    Update main.h

--- src/main.h
+++ src/main.h
@@ -1,65 +1,7 @@
 #ifndef PLUGIN_INCLUDED
 #define PLUGIN_INCLUDED
 
-// ============================================================================
-// 1. WINDOWS & SYSTEM HEADERS
-// ============================================================================
-#ifdef WIN32_LEAN_AND_MEAN
- #undef WIN32_LEAN_AND_MEAN
-#endif
-#define _WIN32_WINNT 0x0600
-#include <windows.h>
-#include <commdlg.h>
-#include <ole2.h>
-#include <shlwapi.h>
-#include <string.h>
-#include <stdio.h>
-#include <math.h>
-
-// ============================================================================
-// 2. PARCHES DE MACROS CONFLICTIVAS
-// ============================================================================
-// Arreglamos el error de DBG (defined no se puede usar así)
-#ifdef _DEBUG
- #define DBG 1
-#else
- #define DBG 0
-#endif
-
-// Definimos V para coefficients.h
-#define V(x) (float)(x)
-
-// Eliminamos parches anteriores de 'copy' para evitar ambigüedad en kali
-// Usaremos funciones inline seguras si hace falta
-
-// ============================================================================
-// 3. PARCHES MATEMÁTICOS (SSE)
-// ============================================================================
-#include <xmmintrin.h>
-#include <emmintrin.h>
-#include <pmmintrin.h>
-
-inline float hsum(__m128 x) {
-    __m128 t = _mm_add_ps(x, _mm_movehl_ps(x, x));
-    __m128 r = _mm_add_ss(t, _mm_shuffle_ps(t, t, 1));
-    float f; _mm_store_ss(&f, r); return f;
-}
-
-template <int i0, int i1, int i2, int i3>
-inline __m128 shuffle(__m128 x) {
-    return _mm_shuffle_ps(x, x, _MM_SHUFFLE(i3, i2, i1, i0));
-}
-
-template <int i0, int i1, int i2, int i3>
-inline __m128 shuffle(__m128 a, __m128 b) {
-    return _mm_shuffle_ps(a, b, _MM_SHUFFLE(i3, i2, i1, i0));
-}
-
-// ============================================================================
-// 4. PARCHES 'SP' Y 'ANALYZER'
-// ============================================================================
-#define tf 
-
+// Definimos la identidad aquí para que todo el plugin la conozca
 #ifndef NAME
  #define NAME "C4 Analyzer"
 #endif
@@ -70,48 +12,19 @@ inline __m128 shuffle(__m128 a, __m128 b) {
  #define VERSION 1.0
 #endif
 
-// ============================================================================
-// 5. INCLUDES DEL PLUGIN
-// ============================================================================
+// Parche mínimo para que 'preset-handler.h' encuentre 'copy'
+#include <string.h>
+#define copy(dest, src, size) memcpy(dest, src, size)
+
+// Includes originales del sistema (Kali se encarga del resto)
 #include "preset-handler.h"
 #include "sa.legacy.h"
 #include "sa.display.h"
 
-// ============================================================================
-// 6. SOBRECARGAS MATEMÁTICAS FINALES
-// ============================================================================
-namespace sp {
-    template <typename T>
-    inline const __m128& as_m128(const T& x) {
-        return *reinterpret_cast<const __m128*>(&x);
-    }
-    
-    inline __m128 operator*(const array<float, 4, SSE>& a, __m128 b) {
-        return _mm_mul_ps(as_m128(a), b);
-    }
-    inline __m128 operator+(const array<float, 4, SSE>& a, __m128 b) {
-        return _mm_add_ps(as_m128(a), b);
-    }
-    inline __m128 max(const array<float, 4, SSE>& a, __m128 b) {
-        return _mm_max_ps(as_m128(a), b);
-    }
-}
-
-// ============================================================================
-// 7. ARREGLO DE AMBIGÜEDAD 'PTR' DE KALI
-// ============================================================================
-// Esto fuerza al compilador a elegir el constructor correcto
-namespace kali {
-    template <typename T>
-    struct PtrFix : Ptr<T> {
-        PtrFix(T* p) : Ptr<T>(p) {}
-    };
-}
-
-// ============================================================================
-// 8. CLASE PLUGIN
-// ============================================================================
-struct Plugin : sp::AlignedNew <16>, PresetHandler <Plugin, 9, sa::config::ParameterCount> {
+struct Plugin :
+    sp::AlignedNew <16>,
+    PresetHandler <Plugin, 9, sa::config::ParameterCount>
+{
     typedef PresetHandler <Plugin, 9, sa::config::ParameterCount> Base;
 
     void suspend() { tf analyzerUpdate(); }
@@ -129,19 +42,22 @@ struct Plugin : sp::AlignedNew <16>, PresetHandler <Plugin, 9, sa::config::Param
         analyzer.update(sampleRate, bpo[shared.settings(bandsPerOctave)]);
     }
 
-    template <typename T> inline_ void process(const T* const* in, T* const* out, int n) {
+    template <typename T> inline_
+    void process(const T* const* in, T* const* out, int n) {
         bypass(in, out, n);
         int ch = shared.settings(sa::settings::inputChannel);
         analyzer.process(in, n, ch);
     }
 
-    template <typename T> inline_ static void bypass(const T* const* in, T* const* out, int n) {
+    template <typename T> inline_
+    static void bypass(const T* const* in, T* const* out, int n) {
         const T* in0 = in[0]; const T* in1 = in[1];
               T* out0 = out[0];       T* out1 = out[1];
         if ((in0 == out0) && (in1 == out1)) return;
         while (--n >= 0) { *out0++ = *in0++; *out1++ = *in1++; }
     }
 
+    // Fix: Eliminamos macros de performance que causan ruido
     void processReplacing(float** in, float** out, int n) {process(in, out, n);}
     #if PROCESS_DBL
     void processDoubleReplacing(double** in, double** out, int n) {process(in, out, n);}
@@ -155,29 +71,42 @@ struct Plugin : sp::AlignedNew <16>, PresetHandler <Plugin, 9, sa::config::Param
         namespace p = parameters;
 
         if (!sa::legacy::convertPreset(value)) return;
+
         bool applyColors = !Settings(prefsKey).get(PrefName()[keepColors], prefs[keepColors].default_);
         int n = applyColors ? Count : ColorsIndex;
         for (int i = 0; i < n; i++) shared.settings(i, value[i + SettingsIndex], false);
+
         analyzerUpdate();
+
         if (shared.editor) shared.editor->settingsChanged(applyColors);
         if (shared.display) shared.display->settingsChanged();
-        else if (Settings(prefsKey).get(PrefName()[smartDisplay], prefs[smartDisplay].default_)) {
-            for (int i = p::w; i <= p::h; i++) shared.parameter[i] = value[i];
+        else {
+             if (Settings(prefsKey).get(PrefName()[smartDisplay], prefs[smartDisplay].default_)) {
+                for (int i = p::w; i <= p::h; i++) shared.parameter[i] = value[i];
+             }
         }
         this->invalidatePreset();
     }
 
-    enum { UniqueID = 'C4Az', Version  = int(VERSION * 1000) };
+    enum {
+        UniqueID = 'C4Az',
+        Version  = int(VERSION * 1000),
+    };
+
     static const char* name()   {return NAME;}
     static const char* vendor() {return COMPANY;}
 
-    ~Plugin() { if (editor) { delete editor; editor = 0; } tf }
+    ~Plugin() {
+        if (editor) { delete editor; editor = 0; }
+        tf
+    }
 
     Plugin(audioMasterCallback master) : Base(master, sa::config::Defaults()) {
         tf
-        this->setNumInputs(2); this->setNumOutputs(2);
-        #if PROCESS_DBL 
-            this->canDoubleReplacing(); 
+        this->setNumInputs(2);
+        this->setNumOutputs(2);
+        #if PROCESS_DBL
+            this->canDoubleReplacing();
         #endif
         shared.analyzer = &analyzer;
         this->setEditor(new vst::Editor<Plugin, sa::Display>(this));
@@ -190,4 +119,5 @@ public:
 private:
     Analyzer analyzer;
 };
-#endif
+
+#endif // ~ PLUGIN_INCLUDED

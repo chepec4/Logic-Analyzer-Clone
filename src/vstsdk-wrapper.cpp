cef1b5c007605d230405d11820192effa1025734
chepec4 <chepec4@gmail.com>
2026-02-13 21:00:05 -0600

    Enhance vstsdk-wrapper with main compatibility function
    Added compatibility function for VSTPluginMain.

--- src/vstsdk-wrapper.cpp
+++ src/vstsdk-wrapper.cpp
@@ -1,15 +1,22 @@
-#include "main.h" // ¡Siempre primero!
+#include "main.h" // Vital que vaya primero
 #include "includes.h"
 
+// Desactivar warnings de Steinberg
 #pragma warning(push, 3)
 #include "audioeffect.cpp"
 #include "audioeffectx.cpp"
 #pragma warning(pop)
 
+// LA CLAVE DE LA VISIBILIDAD
 extern "C" {
     __declspec(dllexport) AEffect* VSTPluginMain(audioMasterCallback master) {
         Plugin* effect = new Plugin(master);
         if (!effect) return 0;
         return effect->getAeffect();
+    }
+
+    // Compatibilidad extra
+    __declspec(dllexport) AEffect* main(audioMasterCallback master) {
+        return VSTPluginMain(master);
     }
 }

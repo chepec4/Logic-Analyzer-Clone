44419691fa15a2ff42b3edaebe989c8ae74282e0
chepec4 <chepec4@gmail.com>
2026-02-14 18:36:41 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,38 @@
-
 #include "includes.h"
 #include "main.h"
 #include "kali/app.dll.h"
 
+
+#ifdef WINDOWS_
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInst, DWORD dwReason, LPVOID /*lpvReserved*/)
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        // Inicializamos los flags de depuración si estamos en modo Debug
+        #ifdef _CRTDBG_MAP_ALLOC
+            _CrtSetDbgFlag(_CRTDBG_CHECK_ALWAYS_DF | _CRTDBG_LEAK_CHECK_DF);
+        #endif
+
+        #if DBG > 1
+            trace.setLevel(trace.Full);
+        #endif
+
+        // Registramos el módulo en el framework Kali
+        kali::app->details_->module_ = hInst;
+    }
+    return TRUE;
+}
+#endif
+
 // ............................................................................
 
-extern "C" AEffect* VSTPluginMain(audioMasterCallback audioMaster)
-{
-    #ifdef _CRTDBG_MAP_ALLOC
-        _CrtSetDbgFlag(_CRTDBG_CHECK_ALWAYS_DF | _CRTDBG_LEAK_CHECK_DF);
-    #endif
-
-    #if DBG > 1
-        trace.setLevel(trace.Full);
-    #endif
-    tf
-
-	if (audioMaster(0, audioMasterVersion, 0, 0, 0, 0))
-	    return (new Plugin(audioMaster))->getAeffect();
-
-    trace("%s: failed, wrong master version.\n", FUNCTION_);
-	return 0;
-}
+/**
+ * NOTA DE INGENIERÍA:
+ * Hemos eliminado la función VSTPluginMain de este archivo para evitar 
+ * conflictos con 'vstsdk-wrapper.cpp'. 
+ * * La comunicación se centraliza en el wrapper para garantizar:
+ * 1. Verificación de versión del Master.
+ * 2. Alineación SSE (sp::AlignedNew).
+ */
 
 // ............................................................................

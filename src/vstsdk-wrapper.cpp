cb6137e06172ee1f3ea445de6aba48a1d322402d
chepec4 <chepec4@gmail.com>
2026-02-14 18:30:06 -0600

    Update vstsdk-wrapper.cpp

--- src/vstsdk-wrapper.cpp
+++ src/vstsdk-wrapper.cpp
@@ -1,10 +1,7 @@
 #include <windows.h>
-#include <commdlg.h>
-#include <objidl.h>
-#include <shlwapi.h>
-
 #include "main.h" 
 #include "includes.h"
+
 
 #pragma warning(push, 3)
 #include "audioeffect.cpp"
@@ -12,10 +9,22 @@
 #pragma warning(pop)
 
 extern "C" {
-    // Solo exportamos VSTPluginMain
+ 
     __declspec(dllexport) AEffect* VSTPluginMain(audioMasterCallback master) {
+        
+        if (!master(0, audioMasterVersion, 0, 0, 0, 0)) return 0;
+
+      
         Plugin* effect = new Plugin(master);
+
         if (!effect) return 0;
+
+     
+        if (effect->getAeffect()->magic != kEffectMagic) {
+            delete effect;
+            return 0;
+        }
+
         return effect->getAeffect();
     }
 }

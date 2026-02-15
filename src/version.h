c05b2c87477db9aaca4aeef17a186bd33cbc0a2f
chepec4 <chepec4@gmail.com>
2026-02-14 22:10:10 -0600

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,32 +1,26 @@
+#ifndef VERSION_INCLUDED
+#define VERSION_INCLUDED
 
-#define VER       1,0,9
-#define NAME      "Spectrum Analyzer"
-#define COMPANY   "Seven Phases"
-#define COPYRIGHT "© Max Mikhailov, 2009-2015"
+// ----------------------------------------------------------------------------
+// IDENTIDAD CORPORATIVA C4
+// ----------------------------------------------------------------------------
+#define VER       1,0,0
+#define NAME      "C4 Analyzer"         // Antes: Spectrum Analyzer
+#define NAME_SHORT "C4Ana"              // Nombre corto para binarios
+#define COMPANY   "C4 Designs"          // Antes: Seven Phases
+#define COPYRIGHT "Copyright (c) 2026 C4 Designs"
 
-// ............................................................................
-
+// ----------------------------------------------------------------------------
+// MACROS DEL SISTEMA (NO TOCAR)
+// ----------------------------------------------------------------------------
 #define PP_STR(A)          PP_STR__(A)
 #define PP_STR_1(A)        PP_STR(A)
 #define PP_STR__(A)        #A
-
 #define PP_CAT(A, B)       PP_CAT__(A, B)
 #define PP_CAT__(A, B)     A##B
-
 #define VERSION__(A, B, C) A.PP_CAT(B, C)
-
 #define VERSION            PP_CAT(VERSION__, (VER))
 #define VERSION_STR        PP_STR_1(VERSION)
 #define VERSION_RC         VER, 0
 
-// ............................................................................
-
-#ifdef PREPROCESS_ONLY
-
-VERSION
-VERSION_RC
-VERSION_STR
-
-#endif // ~ PREPROCESS_ONLY
-
-// ............................................................................
+#endif // VERSION_INCLUDED

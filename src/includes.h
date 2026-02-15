7a1a454812e61851c0e09f71d03b056ab5dc1f3c
chepec4 <chepec4@gmail.com>
2026-02-14 18:53:39 -0600

    Update includes.h

--- src/includes.h
+++ src/includes.h
@@ -1,4 +1,3 @@
-
 #ifndef INCLUDES_INCLUDED
 #define INCLUDES_INCLUDED
 
@@ -7,9 +6,8 @@
 // ............................................................................
 
 #if MACOSX_
-
-#import <Cocoa/Cocoa.h>
-
+    // Soporte para entornos Apple (si se portara el proyecto)
+    #import <Cocoa/Cocoa.h>
 #endif
 
 // ............................................................................
@@ -18,39 +16,49 @@
 
 #pragma warning(push, 3)
 
+// Aseguramos compatibilidad mínima con Windows XP SP2 (Requisito de WDK 7.1)
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x501
+#define _WIN32_WINNT 0x0501
 #endif
 
+// Activamos soporte para temas visuales modernos (Visual Styles)
 #define ISOLATION_AWARE_ENABLED 1
 
-#if 0
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
+/**
+ * DEPURACIÓN DE MEMORIA:
+ * Si se activa DBG en el makefile, habilitamos el rastreador de fugas.
+ */
+#if DBG
+    #ifndef _CRTDBG_MAP_ALLOC
+        #define _CRTDBG_MAP_ALLOC
+    #endif
+    #include <stdlib.h>
+    #include <crtdbg.h>
 #endif
 
 #include <math.h>
 #include <windows.h>
 #include <commctrl.h>
-#include <gl/gl.h>
+#include <gl/gl.h>      // Core de OpenGL para sa.display.h
+#include <gl/glu.h>     // Utilidades de OpenGL
 
+// Limpieza de macros problemáticas de Windows
 #undef small
+#undef min
+#undef max
 
 #pragma warning(pop)
-#pragma warning(disable: 4127) // conditional expression is constant
 
-// enable extra warnings:
-#pragma warning(default: 4191) // unsafe conversion from 'type of expression' to 'type required'
-#pragma warning(default: 4242) // (another) conversion from 'type1' to 'type2', possible loss of data
-#pragma warning(default: 4254) // (another) conversion from 'type1' to 'type2', possible loss of data
-#pragma warning(default: 4263) // member function does not override any base class virtual member function
-#pragma warning(default: 4264) // no override available for virtual member function from base 'class'; function is hidden
-#pragma warning(default: 4265) // class has virtual functions, but destructor is not virtual
-#pragma warning(default: 4266) // no override available for virtual member function from base 'type'; function is hidden
-#pragma warning(default: 4296) // expression is always false
-#pragma warning(default: 4431) // missing type specifier - int assumed
-#pragma warning(default: 4640) // construction of local static object is not thread-safe
+// --- CONFIGURACIÓN DE ADVERTENCIAS METICULOSA ---
+#pragma warning(disable: 4127) // Expresión condicional constante (común en templates)
+#pragma warning(disable: 4512) // No se pudo generar el operador de asignación
+#pragma warning(disable: 4201) // Extensiones no estándar: unión/estructuras anónimas (Crítico para core.h)
+
+// Habilitar advertencias de seguridad extra para el código de audio
+#pragma warning(default: 4191) // Conversión insegura de tipos
+#pragma warning(default: 4242) // Posible pérdida de datos en conversión
+#pragma warning(default: 4265) // Clase con funciones virtuales pero destructor no virtual
+#pragma warning(default: 4640) // Construcción de objeto estático local no segura para hilos
 
 #endif
 

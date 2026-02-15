dcd8f72f7303849309fbbbabf89c2f0db4b9ab51
chepec4 <chepec4@gmail.com>
2026-02-15 09:46:23 -0600

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,26 +1,22 @@
 #ifndef VERSION_INCLUDED
 #define VERSION_INCLUDED
 
-// ----------------------------------------------------------------------------
-// IDENTIDAD CORPORATIVA C4
-// ----------------------------------------------------------------------------
-#define VER       1,0,0
-#define NAME      "C4 Analyzer"         // Antes: Spectrum Analyzer
-#define NAME_SHORT "C4Ana"              // Nombre corto para binarios
-#define COMPANY   "C4 Designs"          // Antes: Seven Phases
+// Definiciones estáticas simples para evitar errores de preprocesador
+#define VER_MAJOR 1
+#define VER_MINOR 0
+#define VER_PATCH 0
+
+// Identidad del Producto
+#define NAME      "C4 Analyzer"
+#define NAME_SHORT "C4Ana"
+#define COMPANY   "C4 Designs"
 #define COPYRIGHT "Copyright (c) 2026 C4 Designs"
 
-// ----------------------------------------------------------------------------
-// MACROS DEL SISTEMA (NO TOCAR)
-// ----------------------------------------------------------------------------
-#define PP_STR(A)          PP_STR__(A)
-#define PP_STR_1(A)        PP_STR(A)
-#define PP_STR__(A)        #A
-#define PP_CAT(A, B)       PP_CAT__(A, B)
-#define PP_CAT__(A, B)     A##B
-#define VERSION__(A, B, C) A.PP_CAT(B, C)
-#define VERSION            PP_CAT(VERSION__, (VER))
-#define VERSION_STR        PP_STR_1(VERSION)
-#define VERSION_RC         VER, 0
+// Versiones pre-calculadas para evitar macros complejas
+#define VERSION     1.00
+#define VERSION_STR "1.00"
+
+// Formato compatible con recursos de Windows (.rc)
+#define VERSION_RC  1,0,0,0
 
 #endif // VERSION_INCLUDED

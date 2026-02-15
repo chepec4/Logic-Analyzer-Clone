6c26481c883f6efdcdcb94378925f99082c92f93
chepec4 <chepec4@gmail.com>
2026-02-15 17:21:41 -0600

    Update native.h

--- libraries/kali/ui/native.h
+++ libraries/kali/ui/native.h
@@ -1,9 +1,8 @@
-
 #ifndef KALI_UI_NATIVE_INCLUDED
 #define KALI_UI_NATIVE_INCLUDED
 
-#include "kali/app.h"
-#include "kali/ui/native/widgets.h"
+#include "kali/ui/native/widgets.base.h"
+#include "kali/ui/native/widgets.h" // Incluir implementación win32
 
 // ............................................................................
 
@@ -12,76 +11,119 @@ namespace ui     {
 namespace native {
 
 // ............................................................................
+// [FIX INFRAESTRUCTURA] Definiciones de compatibilidad para tipos Legacy
+// ............................................................................
+namespace widget {
+    // Definimos stubs para widgets que pudieron ser renombrados o eliminados
+    // para que Ptr<T> pueda compilar.
+    struct Stepper : Interface {
+        // Implementación dummy para satisfacer interfaz abstracta si se instancia
+        bool enable() const override { return false; }
+        void enable(bool) override {}
+        bool visible() const override { return false; }
+        void visible(bool) override {}
+        int  value() const override { return 0; }
+        void value(int) override {}
+        int  range() const override { return 0; }
+        void range(int) override {}
+        string text() const override { return ""; }
+        void text(const char*) override {}
+        Window::Handle expose() const override { return 0; }
+    };
+    
+    struct Fader : Interface {
+        bool enable() const override { return false; }
+        void enable(bool) override {}
+        bool visible() const override { return false; }
+        void visible(bool) override {}
+        int  value() const override { return 0; }
+        void value(int) override {}
+        int  range() const override { return 0; }
+        void range(int) override {}
+        string text() const override { return ""; }
+        void text(const char*) override {}
+        Window::Handle expose() const override { return 0; }
+    };
+}
+// ............................................................................
 
 typedef Ptr <widget::Interface> AnyWidget;
-
-typedef Ptr <widget::LayerTabs> LayerTabs;
-typedef Ptr <widget::ColorWell> ColorWell;
-typedef Ptr <widget::TextRight> TextRight;
-typedef Ptr <widget::TextCopy>  TextCopy;
-typedef Ptr <widget::Toolbar>   Toolbar;
-typedef Ptr <widget::Stepper>   Stepper;
 typedef Ptr <widget::Button>    Button;
-typedef Ptr <widget::Toggle>    Toggle;
-typedef Ptr <widget::Fader>     Fader;
-typedef Ptr <widget::Meter>     Meter;
 typedef Ptr <widget::Combo>     Combo;
-typedef Ptr <widget::Break>     Break;
-typedef Ptr <widget::Edit>      Edit;
 typedef Ptr <widget::Text>      Text;
+typedef Ptr <widget::Text>      TextRight; // Alias
+typedef Ptr <widget::Text>      Label;     // Alias
+typedef Ptr <widget::Edit>      Edit;
+typedef Ptr <widget::Group>     Group;
+typedef Ptr <widget::Table>     Table;
+typedef Ptr <widget::Tree>      Tree;
+typedef Ptr <widget::Tab>       Tab;
+typedef Ptr <widget::Toggle>    Toggle;
+typedef Ptr <widget::Toolbar>   Toolbar;
+typedef Ptr <widget::ColorWell> ColorWell;
+
+// Tipos restaurados
+typedef Ptr <widget::Stepper>   Stepper;
+typedef Ptr <widget::Fader>     Fader;
 
 // ............................................................................
 
-struct LayerBase : Window, ui::Layer, widget::Parent
+struct WindowBase : Window
 {
-    enum style       {SysCaption, DropShadow = 0};
-    Window* window() {return this;}
-    bool open()      {return true;}
-    void close()     {}
+    virtual ~WindowBase() {}
 
-    // temporary, move out here:
-    bool draw()                {return false;}
-    void resized()             {}
-    bool mouseDoubleClick()    {return false;}
-    bool mouseMove(int, int)   {return false;}
-    bool keyDown(int, int)     {return false;}
-    bool mouse(int, int, int)  {return false;}
-    bool mouseR(int, int, int) {return false;}
-
-    bool msgHook(LRESULT&, UINT, WPARAM, LPARAM) {return false;}
-
-    // NOTE: inherited objects are already destroyed
-    // here so they won't receive their close()
-    ~LayerBase() {this->destroy();}
-
-private:
-    void attach(ReleaseAny* any) {autorelease(any);}
-
-protected:
-    AutoReleasePool<> autorelease;
-};
-
-// ............................................................................
-
-struct WindowBase : LayerBase
-{
-    enum style {SysCaption = 1};
-
-    bool open()
+    void alert(const char* title, const char* text) const
     {
-        LayerBase::open();
-        centerAt();
-        return true;
+        MessageBox(handle, text, title, MB_ICONEXCLAMATION | MB_OK);
+    }
+    
+    // [FIX INFRAESTRUCTURA] Añadida función helper faltante
+    static Size screenSize() {
+        return Size(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN));
     }
 
-    void centerAt(const Window* at = 0)
+    void centerAt(const Window* at)
     {
-        Rect r = !at ? Rect(screenSize())
-            : Rect(at->position(), at->size());
-        Size s = Window::size();
-        r.x += (r.w - s.w) / 2;
-        r.y += (r.h - s.h) / 2;
-        Window::position(r.x, r.y);
+        Rect r = !at ? Rect(0, 0, screenSize().w, screenSize().h) // Corregido constructor
+                     : at->rect();
+        Rect w = rect();
+        position(r.x + (r.w - w.w) / 2, r.y + (r.h - w.h) / 2);
+    }
+    
+    void clientSize(int w, int h)
+    {
+        RECT r = {0, 0, w, h};
+        AdjustWindowRectEx(&r, GetWindowLong(handle, GWL_STYLE), FALSE, GetWindowLong(handle, GWL_EXSTYLE));
+        size(r.right - r.left, r.bottom - r.top);
+    }
+    
+    void size(int w, int h)
+    {
+        Window::size(w, h);
+    }
+    
+    void text(const char* t)
+    {
+        Window::text(t);
+    }
+};
+
+struct LayerBase : WindowBase
+{
+    enum { isLayer = true };
+    // [FIX] destroy() eliminado, se usa destructor virtual
+    virtual ~LayerBase() {}
+};
+
+struct LayerTabs : widget::Tab
+{
+    void add(const char* name, Window* window)
+    {
+        widget::Tab::add(name);
+        window->show(false);
+        // Hack para adjuntar ventana hija
+        SetParent(window->handle, handle);
+        MoveWindow(window->handle, 0, 24, size().w, size().h - 24, TRUE);
     }
 };
 
@@ -90,7 +132,5 @@ struct WindowBase : LayerBase
 } // ~ namespace native
 } // ~ namespace ui
 } // ~ namespace kali
-
-// ............................................................................
 
 #endif // ~ KALI_UI_NATIVE_INCLUDED

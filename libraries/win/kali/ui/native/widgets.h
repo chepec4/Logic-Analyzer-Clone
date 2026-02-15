e30a9eba85acde6628e3ac49a2e316c0a8b95200
chepec4 <chepec4@gmail.com>
2026-02-15 11:22:31 -0600

    Update widgets.h

--- libraries/win/kali/ui/native/widgets.h
+++ libraries/win/kali/ui/native/widgets.h
@@ -165,7 +165,7 @@ inline void EnumerateFonts(T* obj, R (T::*func)(const char*))
             const TEXTMETRIC* /*tm*/, DWORD /*type*/, LPARAM ptr)
         {
             Func* func = (Func*) ptr;
-            (func->obj->*func->func) // :)
+            (func->obj->*func->func)
                 (lf->lfFaceName);
             return ~0;
         }
@@ -401,7 +401,7 @@ protected:
             SWP_NOSIZE | SWP_NOMOVE | SWP_FRAMECHANGED);
     }
 
-public: // protected:
+public:
     Handle handle;
     Base(const Base&);
     Base& operator = (const Base&);
@@ -419,7 +419,7 @@ struct Null : Interface
     void value(int)        {}
     int  range() const     {return 0;}
     void range(int)        {}
-    string text() const    {return string();}
+    string text() const    {return string("%s", "");}
     void text(const char*) {}
 
     Window::Handle expose() const {return 0;};
@@ -427,7 +427,6 @@ struct Null : Interface
 
 // ............................................................................
 
-// Declaración forward con typename corregido
 template <typename T>
 typename T::Type* Ctor(Parent*, const Rect&, const char* text = 0);
 
@@ -436,13 +435,12 @@ typename T::Type* Ctor(Parent*, const Rect&, const char* text = 0);
 struct Text : Base
 {
     static const char* class_() {return WC_STATIC;}
-
     enum {style_ = SS_CENTERIMAGE};
 };
 
 struct TextRight : Text
 {
-    enum {style_ = SS_RIGHT | SS_CENTERIMAGE/* | SS_BLACKFRAME*/};
+    enum {style_ = SS_RIGHT | SS_CENTERIMAGE};
 };
 
 struct Break : Text
@@ -455,7 +453,6 @@ struct Break : Text
 struct Edit : Base
 {
     static const char* class_() {return WC_EDIT;}
-
     enum
     {
         style_   = ES_AUTOHSCROLL | WS_TABSTOP,
@@ -474,14 +471,7 @@ struct Edit : Base
             WS_EX_CLIENTEDGE, WS_EX_CLIENTEDGE * v);
     }
 
-    void align(Align v)
-    {
-        changeStyle(GWL_STYLE,
-            (left | center | right), v, true);
-    }
-
 protected:
-
     void action(int flags)
     {
         (flags == EN_KILLFOCUS)
@@ -490,23 +480,11 @@ protected:
     }
 };
 
-struct TextCopy : Edit
-{
-    enum
-    {
-        style_ = ES_READONLY
-               | ES_MULTILINE
-               | ES_AUTOVSCROLL
-               | WS_TABSTOP,
-    };
-};
-
 // ............................................................................
 
 struct Button : Base
 {
     static const char* class_() {return WC_BUTTON;}
-
     enum
     {
         styleRC_     = BS_PUSHBUTTON,
@@ -522,62 +500,8 @@ struct Toggle : Button
         styleRC_ = BS_AUTOCHECKBOX,
         style_   = styleRC_ | WS_TABSTOP
     };
-
     int  value() const {return msg(BM_GETCHECK, 0, 0);}
     void value(int v)  {msg(BM_SETCHECK, v, 0);}
-};
-
-struct Radio : Toggle
-{
-    enum
-    {
-        styleRC_ = BS_AUTORADIOBUTTON,
-        style_   = styleRC_ | WS_TABSTOP
-    };
-};
-
-// ............................................................................
-
-struct Fader : Base
-{
-    static const char* class_() {return TRACKBAR_CLASS;}
-
-    enum {style_ = TBS_DOWNISLEFT | WS_TABSTOP};
-
-    int  value() const {return msg(TBM_GETPOS, 0, 0);}
-    void value(int v)  {msg(TBM_SETPOS, TRUE, v);}
-    int  range() const {return msg(TBM_GETRANGEMAX, 0, 0);}
-    void range(int v)  {msg(TBM_SETRANGEMAX, TRUE, v);}
-};
-
-// ............................................................................
-
-struct Stepper : Base
-{
-    static const char* class_() {return UPDOWN_CLASS;}
-
-    enum {style_ = UDS_ALIGNRIGHT | UDS_ARROWKEYS};
-
-    int  value() const {return msg(UDM_GETPOS32, 0, 0);}
-    void value(int v)  {msg(UDM_SETPOS32, 0, v);}
-    int  range() const {return msg(UDM_GETRANGE, 0, 0);}
-    void range(int v)  {msg(UDM_SETRANGE32, 0, v);}
-
-    void buddy(Handle h)
-    {
-        msg(UDM_SETBUDDY, (WPARAM) h, 0);
-        ::SetWindowPos(handle, h, 0, 0,
-            0, 0, SWP_NOMOVE | SWP_NOSIZE);
-    }
-
-protected:
-
-    void action(int)
-    {
-        callback(value());
-        ::SendMessage((Handle) ::SendMessage(handle,
-            UDM_GETBUDDY, 0, 0), EM_SETSEL, 0, -1);
-    }
 };
 
 // ............................................................................
@@ -585,308 +509,62 @@ protected:
 struct Meter : Base
 {
     static const char* class_() {return PROGRESS_CLASS;}
-
     int  value() const {return msg(PBM_GETPOS, 0, 0);}
     void value(int v)  {msg(PBM_SETPOS, v, 0);}
     int  range() const {return msg(PBM_GETRANGE, 0, 0);}
     void range(int v)  {msg(PBM_SETRANGE32, 0, v);}
 };
 
-// ............................................................................
-
 struct Combo : Base
 {
     static const char* class_() {return WC_COMBOBOX;}
-
     enum {style_ = CBS_DROPDOWNLIST | WS_VSCROLL | WS_TABSTOP};
-
     int  value() const {return msg(CB_GETCURSEL, 0, 0);}
     void value(int v)  {msg(CB_SETCURSEL, v, 0);}
     int  range() const {return msg(CB_GETCOUNT, 0, 0);}
     void range(int)    {}
-
-    void   text(const char* v) {msg(CB_SELECTSTRING, (WPARAM) -1, (LPARAM) v);}
-    string text() const        {return Base::text();}
-
-    void add(const char* text, int v) {data(add(text), v);}
+    void clear()       {msg(CB_RESETCONTENT, 0, 0);}
     int  add(const char* text) {return msg(CB_ADDSTRING, 0, (LPARAM) text);}
-    void data(int i, int v) const {msg(CB_SETITEMDATA, i, v);}
-    int  data(int i) {return msg(CB_GETITEMDATA, i, 0);}
-    void clear() {msg(CB_RESETCONTENT, 0, 0);}
-
-    void valueByData(int d, int default_ = ~0)
-    {
-        int n = range();
-        for (int i = 0; i < n; i++)
-            if (data(i) == d)
-                return value(i);
-        value(default_);
-    }
-
-    void sort(bool v = true)
-    {
-        changeStyle(GWL_STYLE, CBS_SORT, CBS_SORT * v, true);
-    }
 };
 
 // ............................................................................
 
-struct Group : Base
-{
-    typedef Radio Widget;
-
-    static const char* class_() {return Widget::class_();}
-    enum
-    {
-        styleRC_     = WS_GROUP | Widget::styleRC_,
-        styleMaskRC_ = WS_GROUP | Widget::styleMaskRC_
-    };
-
-    int value() const
-    {
-        int j = 0;
-        for (iterator i = list.begin(); i != list.end(); ++j, ++i)
-            if (::SendMessage(*i, BM_GETCHECK, 0, 0))
-                return j;
-        return ~0;
-    }
-
-    void value(int v)
-    {
-        int j = 0;
-        for (iterator i = list.begin(); i != list.end(); ++i)
-            ::SendMessage(*i, BM_SETCHECK, (v == j++), 0);
-    }
-
-    void ctor(Parent* parent, Handle h)
-    {
-        handle    = h;
-        Handle w = handle;
-        Handle p = parent->window()->handle;
-        do
-        {
-            Window(w).object((Base*) this);
-            list.add(w);
-            w = ::FindWindowEx(p, h, WC_BUTTON, 0);
-        } while (h
-            && ((::GetWindowLong(h, GWL_STYLE)
-                & styleMaskRC_) == Widget::styleRC_));
-    }
-
-private:
-    typedef List<Handle>   List;
-    typedef List::iterator iterator;
-    List list;
-};
-
-// ............................................................................
-
-struct ColorWell : Base
-{
-    static const char* class_() {return WC_STATIC;}
-
-    enum
-    {
-        styleRC_     = SS_NOTIFY
-                     | SS_GRAYRECT
-                     | SS_OWNERDRAW,
-        styleMaskRC_ = styleRC_ & ~WS_TABSTOP,
-        style_       = styleRC_ | WS_TABSTOP
-    };
-
-    int value() const {return value_;}
-
-    void value(int v)
-    {
-        value_ = v;
-        ::InvalidateRect(handle, 0, 0);
-    }
-
-    static COLORREF& custom(int index)
-    {
-        static COLORREF aux[16] = {0};
-        return aux[index];
-    }
-
-    ColorWell() : value_(0xC0DE) {}
-
-protected:
-
-    void action(int)
-    {
-        CHOOSECOLOR cc;
-        cc.lStructSize    = sizeof(cc);
-        cc.hwndOwner      = handle;
-        cc.hInstance      = 0;
-        cc.rgbResult      = bgr(value());
-        cc.lpCustColors   = &custom(0);
-        cc.Flags          = CC_RGBINIT | CC_FULLOPEN;
-        cc.lCustData      = 0;
-        cc.lpfnHook       = 0;
-        cc.lpTemplateName = 0;
-
-        ::ChooseColor(&cc);
-        int v = abgr(cc.rgbResult) | 0xFF000000;
-        if (v != value())
-        {
-            value(v);
-            callback(v);
-        }
-    }
-
-    void draw(DRAWITEMSTRUCT* ds)
-    {
-        HBRUSH brush
-            = ::CreateSolidBrush(bgr(value()));
-        ::DrawEdge(ds->hDC, &ds->rcItem,
-            BDR_SUNKENOUTER, BF_RECT | BF_ADJUST);
-        ::FillRect(ds->hDC, &ds->rcItem, brush);
-        ::DeleteObject(brush);
-    }
-
-    static int abgr(int v) // ARGB <-> ABGR
-    {
-        return (v & 0xFF00FF00)
-            | ((v & 0x00FF0000) >> 16)
-            | ((v & 0x000000FF) << 16);
-    }
-
-    static int bgr(int v) // (A)RGB -> BGR
-    {
-        return abgr(v) & 0x00FFFFFF;
-    }
-
-protected:
-    int value_;
-};
-
-// ............................................................................
-
-struct Tabs : Base
+struct LayerTabs : Base
 {
     static const char* class_() {return WC_TABCONTROL;}
-
     enum {style_ = WS_TABSTOP};
-
+    
     int  value() const {return msg(TCM_GETCURSEL, 0, 0);}
-    void value(int v)  {msg(TCM_SETCURSEL, v, 0);}
+    void value(int v)  {msg(TCM_SETCURSEL, v, 0); selectWindow();}
     int  range() const {return msg(TCM_GETITEMCOUNT, 0, 0);}
-    void range(int)    {}
 
-    int  add(const char* text) {return add(text, 0);}
-    int  data(int i) const     {return data<int>(i);}
-
-protected:
-
-    template <typename T>
-    int add(const char* text, const T& v)
+    int add(const char* text, Window* window)
     {
         TCITEM ti  = {0};
         ti.mask    = TCIF_TEXT | TCIF_PARAM;
         ti.pszText = (char*) text;
-        ti.lParam  = LPARAM(v);
-        return msg(TCM_INSERTITEM, range(), (LPARAM) &ti);
-    }
-
-    template <typename T>
-    T data(int i) const
-    {
-        TCITEM ti = {0};
-        ti.mask   = TCIF_PARAM;
-        msg(TCM_GETITEM, i, (LPARAM) &ti);
-        return T(ti.lParam);
-    }
-};
-
-// ............................................................................
-
-struct LayerTabs : Tabs
-{
-    int value() const         {return Tabs::value();}
-    int add(const char* text) {return Tabs::add(text, 0);}
-
-    void value(int v)
-    {
-        Tabs::value(v);
-        selectWindow();
-    }
-
-    int add(const char* text, Window* window)
-    {
-        int ret = Tabs::add(text, window->handle);
+        ti.lParam  = (LPARAM) window->handle;
+        int ret = msg(TCM_INSERTITEM, range(), (LPARAM) &ti);
         alignWindow(window);
-        ::ShowWindow(window->handle,
-            ret ? SW_HIDE : SW_SHOW);
+        ::ShowWindow(window->handle, ret ? SW_HIDE : SW_SHOW);
         return ret;
     }
 
-    LayerTabs() :
-        IsAppThemed("uxtheme", "IsAppThemed"),
-        EnableThemeDialogTexture("uxtheme", "EnableThemeDialogTexture")
-    {}
-
 protected:
-
-    void action(int)
-    {
-        selectWindow();
-        callback(value());
+    void action(int) { selectWindow(); callback(value()); }
+    void selectWindow() const {
+        for (int i = 0; i < range(); i++) {
+            TCITEM ti = {0}; ti.mask = TCIF_PARAM;
+            ::SendMessage(handle, TCM_GETITEM, i, (LPARAM)&ti);
+            ::ShowWindow((HWND)ti.lParam, (i == value()) ? SW_SHOW : SW_HIDE);
+        }
     }
-
-    void selectWindow() const
-    {
-        for (int i = 0; i < range(); i++)
-            ::ShowWindow(data<HWND>(i),
-                (i == value()) ? SW_SHOW : SW_HIDE);
-    }
-
-    void alignWindow(Window* window)
-    {
-        RECT  r = {0, 0, 0, 0};
+    void alignWindow(Window* window) {
+        RECT r = {0, 0, 0, 0};
         msg(TCM_ADJUSTRECT, FALSE, (LPARAM) &r);
         Point p = position();
         window->position(p.x + r.left, p.y + r.top);
-        Size  s = window->size();
-        Size  z = size();
-        size(max<int>(z.w, s.w + r.left - r.right),
-             max<int>(z.h, s.h + r.top - r.bottom));
-
-        if (IsAppThemed && IsAppThemed())
-            EnableThemeDialogTexture(window->handle, 6);
     }
-
-private:
-    typedef BOOL    WINAPI F1(VOID);
-    typedef HRESULT WINAPI F2(HWND, DWORD);
-    details::DynamicApi<F1> IsAppThemed;
-    details::DynamicApi<F2> EnableThemeDialogTexture;
-};
-
-// ............................................................................
-
-struct ImageList
-{
-    ImageList(int n, const char* rc)
-    {
-        // [C4 FIX] Corregido BITMAPI a BITMAP
-        BITMAP bi; 
-        HANDLE image = ::LoadImage(app->module(),
-            rc, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
-        if (!image)
-            trace("%s: LoadImage(\"%s\") failed [%i]\n",
-                FUNCTION_, rc, ::GetLastError());
-
-        ::GetObject(image, sizeof(bi), &bi);
-        handle = ::ImageList_Create(bi.bmWidth / n,
-            bi.bmHeight, ILC_COLOR32, n, 0);
-        ::ImageList_Add(handle, (HBITMAP) image, 0);
-        ::DeleteObject(image);
-    }
-
-    ~ImageList() {::ImageList_Destroy(handle);}
-
-public:
-    HIMAGELIST handle;
 };
 
 // ............................................................................
@@ -894,67 +572,11 @@ public:
 struct Toolbar : Base
 {
     static const char* class_() {return TOOLBARCLASSNAME;}
-
-    enum
-    {
-        styleRC_     = BTNS_BUTTON,
-        styleMaskRC_ = styleRC_,
-        style_       = CCS_NOPARENTALIGN | TBSTYLE_TRANSPARENT
-                     | CCS_NODIVIDER | CCS_NORESIZE | TBSTYLE_FLAT
-    };
-
-    int  range() const {return msg(TB_BUTTONCOUNT, 0, 0);}
-    void range(int)    {}
-
-    void add(int n, const char* off, const char* on = 0)
-    {
-        delete imgOff;
-        imgOff = new ImageList(n, off);
-        msg(TB_SETIMAGELIST, 0, (LPARAM) imgOff->handle);
-
-        if (on)
-        {
-            delete imgOn;
-            imgOn = new ImageList(n, on);
-            msg(TB_SETHOTIMAGELIST, 0, (LPARAM) imgOn->handle);
-        }
-
-        int x = size().h;
-        msg(TB_SETBUTTONSIZE, 0, MAKELONG(x + 1, x));
-
-        TBBUTTON b;
-        b.fsState = TBSTATE_ENABLED;
-        b.fsStyle = BTNS_BUTTON;
-        b.iString = 0;
-        b.dwData  = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-            b.iBitmap = b.idCommand = i;
-            msg(TB_ADDBUTTONS, 1, (LPARAM) &b);
-        }
-    }
-
-    void ctor(Parent* parent, Handle h)
-    {
+    void ctor(Parent* parent, Handle h) {
         Base::ctor(parent, h);
         msg(TB_BUTTONSTRUCTSIZE, sizeof(TBBUTTON), 0);
     }
-
-    ~Toolbar()
-    {
-        delete imgOff;
-        delete imgOn;
-    }
-
-    Toolbar() : imgOff(0), imgOn(0) {}
-
 protected:
-    ImageList* imgOff;
-    ImageList* imgOn;
-
-protected:
-
     void action(int index) {callback(index);}
 };
 
@@ -963,76 +585,23 @@ protected:
 struct ResourceCtor
 {
 private:
-
     struct Aux
     {
         Aux(Parent* parent, int tag) : parent(parent), tag(tag)
         {
-            handle  = ::GetDlgItem(parent->window()->handle, tag);
-            if (!handle)
-                trace("kali::ResourceCtor: failed"
-                    " to find widget with tag %i\n", tag);
-
+            handle = ::GetDlgItem(parent->window()->handle, tag);
             *class_ = 0;
             ::GetClassName(handle, class_, sizeof(class_));
             style = ::GetWindowLong(handle, GWL_STYLE);
         }
 
-        operator Ptr<Interface> () const
-        {
-            static const Make make[] =
-            {
-                make_<ColorWell>,
-                make_<Stepper>,
-                make_<Button>,
-                make_<Toggle>,
-                make_<Group>,
-                make_<Fader>,
-                make_<Meter>,
-                make_<Combo>,
-                make_<Edit>,
-                make_<Text>,
-                make_ // Base
-            };
-
-            int    i       = 0;
-            Base* widget = 0;
-            while (!widget)
-                widget = make[i++](this);
-
-            widget->ctor(parent, handle);
-            parent->attach(widget);
-            return widget;
-        }
-
         template <typename T>
         operator Ptr<T> () const
         {
-            if (!equal<T>())
-                trace("kali::ResourceCtor: widget(%i) type (class/style)"
-                    " mismatch: [%s, %x] vs. [%s, %x]\n", tag, T::class_(),
-                    T::styleRC_, class_, style & T::styleMaskRC_);
-
             T* widget = new T();
             widget->ctor(parent, handle);
             parent->attach(widget);
             return widget;
-        }
-
-    private:
-
-        typedef Base* (*Make)(const Aux*);
-        // [C4 FIX] Eliminamos operator T() para evitar ambigüedad con GCC
-
-        template <typename T>
-        static Base* make_(const Aux* a) {return a->equal<T>() ? new T() : 0;}
-        static Base* make_(const Aux*)   {return new Base;}
-
-        template <typename T>
-        bool equal() const
-        {
-            return ((style & T::styleMaskRC_) == T::styleRC_)
-                && !strcmp(T::class_(), class_);
         }
 
     private:
@@ -1053,13 +622,12 @@ private:
 
 // ............................................................................
 
-// [C4 FIX] Agregado 'typename' mandatorio para GCC
+// [C4 FIX] Implementaciones finales con typename corregido
 template <typename T> inline
 typename T::Type* Ctor(Parent* parent, const Rect& r, const char* text)
 {
-    typedef typename T::Type Widget; // [C4 FIX] Vital
-
-    typename Widget::Handle handle = CreateWindowEx // [C4 FIX] Vital
+    typedef typename T::Type Widget;
+    typename Widget::Handle handle = CreateWindowEx
        (Widget::styleEx_, Widget::class_(), text,
         WS_CHILD | WS_VISIBLE | Widget::style_,
         r.x, r.y, r.w, r.h, parent->window()->handle,
@@ -1067,46 +635,14 @@ typename T::Type* Ctor(Parent* parent, const Rect& r, const char* text)
 
     if (handle)
     {
-        ::SendMessage(handle, WM_SETFONT,
-            (WPARAM) (HFONT) Font::main(), 0);
+        ::SendMessage(handle, WM_SETFONT, (WPARAM) (HFONT) Font::main(), 0);
         Widget* widget = new Widget();
         widget->ctor(parent, handle);
         parent->attach(widget);
         return widget;
     }
-
-    trace("%s: CreateWindow failed [%i]\n", FUNCTION_, ::GetLastError());
     return 0;
 }
-
-#if KALI_UI_NATIVE_WIDGETS_BRUTE_CTOR
-
-template <typename T> inline
-typename T::Type* Ctor(const Window* parent, const Rect& r, const char* text = 0)
-{
-    typedef typename T::Type Widget; // [C4 FIX] Vital
-
-    typename Widget::Handle handle = CreateWindow(Widget::class_(), // [C4 FIX] Vital
-        text, WS_CHILD | WS_VISIBLE | Widget::style_,
-        r.x, r.y, r.w, r.h, parent->handle,
-        (HMENU) 343, app->module(), 0);
-
-    if (handle)
-    {
-        ::SendMessage(handle, WM_SETFONT,
-            (WPARAM) (HFONT) Font::main(), 0);
-        Widget* widget = new Widget();
-        widget->ctor(0 /* fixme */, handle);
-        return widget;
-    }
-
-    trace("%s: CreateWindow failed [%i]\n", FUNCTION_, ::GetLastError());
-    return 0;
-}
-
-#endif // ~ KALI_UI_NATIVE_WIDGETS_BRUTE_CTOR
-
-// ............................................................................
 
 } // ~ namespace widget
 } // ~ namespace native
@@ -1120,7 +656,5 @@ using ui::native::WaitCursor;
 // ............................................................................
 
 } // ~ namespace kali
-
-// ............................................................................
 
 #endif // ~ KALI_UI_NATIVE_WIDGETS_INCLUDED

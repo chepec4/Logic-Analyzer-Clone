8a237c5f3f3ac842bae37b77efd4ee7d464c0928
chepec4 <chepec4@gmail.com>
2026-02-16 13:34:19 -0600

    Update app.dll.h

--- libraries/win/kali/app.dll.h
+++ libraries/win/kali/app.dll.h
@@ -1,31 +1,39 @@
-
 #ifndef KALI_APP_DLL_INCLUDED
 #define KALI_APP_DLL_INCLUDED
-#if defined(_DLL) | defined(_WINDLL)
+
+// Este archivo se incluye en main.cpp para definir el Entry Point de la DLL.
+// Solo se activa si se compila como DLL.
+
+#if defined(_DLL) || defined(_WINDLL) || defined(WIN32)
 
 // ............................................................................
 
-BOOL WINAPI DllMain(HMODULE module, DWORD reason, LPVOID)
+extern "C" BOOL WINAPI DllMain(HMODULE module, DWORD reason, LPVOID)
 {
     using namespace kali;
 
     switch (reason)
     {
         case DLL_PROCESS_ATTACH:
+            // Inicialización del Singleton de la App
             app.alloc();
+            // Inicialización de detalles internos
             app->details_ = app->autorelease(new AppDetails);
             app->details_->module_ = module;
+            
+            // Opcional: DisableThreadLibraryCalls(module) para optimizar
             break;
 
         case DLL_PROCESS_DETACH:
+            // Limpieza ordenada
             app.release();
             break;
     }
 
-	return TRUE;
+    return TRUE;
 }
 
 // ............................................................................
 
-#endif // ~ _DLL | _WINDLL
+#endif 
 #endif // ~ KALI_APP_DLL_INCLUDED

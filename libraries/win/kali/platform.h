6ef7d7ac26afb1b634ec4b441c90cf6a39064e23
chepec4 <chepec4@gmail.com>
2026-02-16 13:30:18 -0600

    Update platform.h

--- libraries/win/kali/platform.h
+++ libraries/win/kali/platform.h
@@ -1,23 +1,43 @@
-
 #ifndef KALI_PLATFORM_INCLUDED
 #define KALI_PLATFORM_INCLUDED
 
 // ............................................................................
 
-#define WINDOWS_     1 // actually means MSVC
+#define WINDOWS_ 1
 
-#define API_EXPORT __declspec(dllexport)
+// DLL Export
+#if defined(_MSC_VER)
+    #define API_EXPORT __declspec(dllexport)
+#else
+    #define API_EXPORT __attribute__((dllexport))
+#endif
+
+// Function Metadata
 #define FUNCTION_  __FUNCTION__
-#define FUNCSIG_   __FUNCSIG__
 
-#define inline_    __forceinline
-#define restrict_  __restrict
-#define noalias_   __declspec(noalias)
+#if defined(__GNUC__) || defined(__clang__)
+    #define FUNCSIG_   __PRETTY_FUNCTION__
+#elif defined(_MSC_VER)
+    #define FUNCSIG_   __FUNCSIG__
+#else
+    #define FUNCSIG_   __FUNCTION__
+#endif
 
-#define align_(x)  __declspec(align(x))
-
-#define format__
-#define vsnprintf   _vsnprintf
+// Inline & Restrict
+#if defined(_MSC_VER)
+    #define inline_    __forceinline
+    #define restrict_  __restrict
+    #define noalias_   __declspec(noalias)
+    #define align_(x)  __declspec(align(x))
+    #define format__
+    #define vsnprintf  _vsnprintf
+#else
+    #define inline_    __attribute__((always_inline)) inline
+    #define restrict_  __restrict__
+    #define noalias_   
+    #define align_(x)  __attribute__((aligned(x)))
+    #define format__   __attribute__((format(printf, 2, 3)))
+#endif
 
 // ............................................................................
 

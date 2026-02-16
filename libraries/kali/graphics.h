163b028e63eca75bc2b5f67b3e96351a310916bd
chepec4 <chepec4@gmail.com>
2026-02-16 13:01:33 -0600

    Update graphics.h

--- libraries/kali/graphics.h
+++ libraries/kali/graphics.h
@@ -1,19 +1,14 @@
-
 #ifndef KALI_GRAPHICS_INCLUDED
 #define KALI_GRAPHICS_INCLUDED
 
-// ............................................................................
-
 #include "kali/platform.h"
 
-#if      WINDOWS_
+#if WINDOWS_
 #include "kali/graphics.gdiplus.h"
-#elif    MACOSX_
+#elif MACOSX_
 #include "kali/graphics.cocoa.h"
 #else
-#error   not implemented.
+#error not implemented.
 #endif
-
-// ............................................................................
 
 #endif // ~ KALI_GRAPHICS_INCLUDED

e115cc63087322acd6cb105af9b9ef9ba864ca05
chepec4 <chepec4@gmail.com>
2026-02-16 12:57:14 -0600

    Update types.h

--- libraries/kali/types.h
+++ libraries/kali/types.h
@@ -1,57 +1,38 @@
-
 #ifndef KALI_TYPES_INCLUDED
 #define KALI_TYPES_INCLUDED
 
 #include "kali/platform.h"
 
-// ............................................................................
-
 namespace kali {
-
-// ............................................................................
 
 enum Align  {left, center, right};
 enum UpDown {up, down};
 enum Weight {regular, bold};
 
-// ............................................................................
-
 struct Null {};
 struct Undefined;
-
-// ............................................................................
 
 struct ReleaseAny
 {
     virtual ~ReleaseAny() {}
 };
 
-// ............................................................................
-
 template <typename T>
 struct Ptr
 {
     typedef T Type;
 
-    Ptr(Type* ptr = 0) : ptr(ptr) {}
-    operator Type*    () const {return   ptr;}
+    Ptr(Type* ptr = nullptr) : ptr(ptr) {}
+    operator Type* () const {return   ptr;}
     operator Type&    () const {return  *ptr;}
     operator bool     () const {return !!ptr;}
-    Type& operator *  () const {return  *ptr;}
+    Type& operator * () const {return  *ptr;}
     Type* operator -> () const {return   ptr;}
-
-    #if 0
-    // hmm, well, weird, but TOO sweet syntax sugar:
-    template <typename U>
-    void operator += (const U& u) const {*ptr += u;}
-    #endif
 
 private:
     Type* ptr;
     template <typename U> operator U () const;
 };
-
-// ............................................................................
 
 template <typename T, int Unique = 0>
 struct singleton
@@ -70,8 +51,10 @@ struct singleton
 
     static void release()
     {
-        delete ptr;
-        ptr = 0;
+        if (ptr) {
+            delete ptr;
+            ptr = nullptr;
+        }
     }
 
 private:
@@ -80,12 +63,8 @@ private:
 };
 
 template <typename T, int Unique>
-T* singleton<T, Unique>::ptr = 0;
-
-// ............................................................................
+T* singleton<T, Unique>::ptr = nullptr;
 
 } // ~ namespace kali
-
-// ............................................................................
 
 #endif // KALI_TYPES_INCLUDED

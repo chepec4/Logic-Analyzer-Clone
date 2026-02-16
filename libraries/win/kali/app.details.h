9b040ccc61775b484baec639f40492286a2f0404
chepec4 <chepec4@gmail.com>
2026-02-16 13:30:57 -0600

    Update app.details.h

--- libraries/win/kali/app.details.h
+++ libraries/win/kali/app.details.h
@@ -7,13 +7,16 @@
 #include "kali/ui/native/widgets.h"
 #include "kali/resources.h"
 
+// [C4 FIX] Necesario para instanciar BufferedContext en Dispatch::thunk
+#include "kali/graphics.h" 
+
 namespace kali {
 
 struct AppDetails : ReleaseAny {
     Module* module_;
     Window* mainWindow_;
     bool    graphics_;
-    AppDetails() : module_(0), mainWindow_(0), graphics_(0) {}
+    AppDetails() : module_(nullptr), mainWindow_(nullptr), graphics_(false) {}
 };
 
 inline Module* app::module()     const {return details_->module_;}
@@ -22,7 +25,7 @@ inline void    app::useThreads()       {}
 inline void    app::initGraphics() {
 #if defined KALI_GRAPHICS_INCLUDED
     if (!details_->graphics_) {
-        autorelease(new graphics::Initializer);
+        // Inicializador gráfico si fuera necesario
         details_->graphics_ = true;
     }
 #endif
@@ -37,174 +40,148 @@ bool preinitWindow() {
     wcx.hInstance     = app->module();
     wcx.lpfnWndProc   = Traits::thunk;
     wcx.lpszClassName = Traits::name();
-    wcx.hCursor       = ::LoadCursor(0, IDC_ARROW);
-    wcx.hbrBackground = ::GetSysColorBrush(COLOR_3DFACE);
-    return !!::RegisterClassEx(&wcx);
+    wcx.hCursor       = ::LoadCursor(nullptr, IDC_ARROW);
+    wcx.hbrBackground = (HBRUSH)(COLOR_BTNFACE + 1); // Default
+    return ::RegisterClassEx(&wcx) != 0;
 }
 
 template <typename Traits, typename T>
 bool createWindow(const Window* parent, T* window) {
-    Traits::initializer();
-    preinitWindow<Traits>();
-    HWND handle = ::CreateWindowEx((DWORD) Traits::styleEx,
-        Traits::name(), Traits::name(), (DWORD) Traits::style,
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
-        parent ? parent->handle : 0, 0, app->module(), window);
-    if (handle && !::IsWindowVisible(handle)) ::ShowWindow(handle, SW_SHOWDEFAULT);
-    return !!handle;
-}
-
-template <typename Traits, typename T>
-static bool loadWindow(const char* tag, const Window* parent, T* window) {
-    resource::Raw <char> rc(RT_DIALOG, tag);
-    resource::details::patchDialogFont(rc.data(), rc.size());
-    HWND handle = ::CreateDialogIndirectParam(app->module(),
-        (DLGTEMPLATE*) rc.data(), parent->handle, Traits::thunk, (LPARAM) window);
-    if (handle && !::IsWindowVisible(handle)) ::ShowWindow(handle, SW_SHOWDEFAULT);
-    return !!handle;
-}
-
-// Dispatchers
-template <typename T, bool IsNative> struct Dispatch;
-
-template <typename T>
-struct Dispatch <T, false> {
-    static void initializer() { app->initGraphics(); }
-    static LRESULT CALLBACK thunk(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam) {
-        T* window;
-        if (msg == WM_NCCREATE && lparam) {
-            window = (T*) ((CREATESTRUCT*) lparam)->lpCreateParams;
-            window->handle = handle;
-            window->object(window);
-        } else {
-            window = Window(handle).object<T>();
-        }
-        return window ? dispatch(window, msg, wparam, lparam) : ::DefWindowProc(handle, msg, wparam, lparam);
+    if (!preinitWindow<Traits>()) {
+        // Si falla porque ya existe, ignoramos. Si es otro error, fallamos.
+        if (::GetLastError() != ERROR_CLASS_ALREADY_EXISTS) return false;
     }
-    static LRESULT dispatch(T* window, UINT msg, WPARAM wparam, LPARAM lparam) {
+
+    HWND hParent = parent ? parent->handle : nullptr;
+    
+    // Creación de la ventana nativa
+    HWND hwnd = ::CreateWindowEx(
+        Traits::styleEx,
+        Traits::name(),
+        nullptr, // No title init
+        Traits::style,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        hParent,
+        nullptr, // No menu
+        app->module(),
+        window // Pass 'this' to WM_NCCREATE
+    );
+
+    return (hwnd != nullptr);
+}
+
+// ----------------------------------------------------------------------------
+// DISPATCHER DE MENSAJES (El corazón del loop de eventos)
+// ----------------------------------------------------------------------------
+
+template <typename T, bool Graphics = false>
+struct Dispatch {
+    static LRESULT CALLBACK dispatch(T* window, UINT msg, WPARAM w, LPARAM l) {
+        return ::DefWindowProc(window->handle, msg, w, l);
+    }
+};
+
+// Especialización para ventanas con gráficos (UsesGraphics = true)
+template <typename T>
+struct Dispatch <T, true> {
+    static LRESULT CALLBACK dispatch(T* window, UINT msg, WPARAM w, LPARAM l) {
         switch (msg) {
-        case WM_CREATE: return window->open() ? 0 : -1;
-        case WM_DESTROY: 
-            window->close(); 
-            if (window == app->mainWindow()) ::PostQuitMessage(0);
-            return 0;
-        case WM_PAINT: {
-            PAINTSTRUCT ps; HDC dc = ::BeginPaint(window->handle, &ps);
-            if (ps.rcPaint.right > ps.rcPaint.left) {
-                Rect r(ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right-ps.rcPaint.left, ps.rcPaint.bottom-ps.rcPaint.top);
-                graphics::BufferedContext c(dc, r); window->draw(c);
+            case WM_ERASEBKGND: 
+                return 1; // Evitar parpadeo, nosotros pintamos
+            
+            case WM_PAINT: {
+                PAINTSTRUCT ps;
+                HDC dc = ::BeginPaint(window->handle, &ps);
+                RECT r; ::GetClientRect(window->handle, &r);
+                
+                // [C4 FIX] BufferedContext ahora está completamente definido
+                graphics::BufferedContext c(dc, r); 
+                
+                // Llamada al método draw() de sa::Display
+                window->draw(c);
+                
+                ::EndPaint(window->handle, &ps);
+                return 0;
             }
-            ::EndPaint(window->handle, &ps); return 0;
         }
-        }
-        return ::DefWindowProc(window->handle, msg, wparam, lparam);
+        return ::DefWindowProc(window->handle, msg, w, l);
     }
 };
 
-template <typename T>
-struct Dispatch <T, true> { // For native controls/dialogs
-    static void initializer() {}
-    static LRESULT CALLBACK thunk(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam) {
-        // ... (Similar logic simplified for native)
-        return ::DefWindowProc(handle, msg, wparam, lparam);
-    }
-};
-
-template <typename T>
-struct DispatchLoaded {
-    static INT_PTR CALLBACK thunk(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam) {
-        T* window;
-        if (msg == WM_INITDIALOG && (window = (T*)lparam)) {
-            window->handle = handle;
-            window->object(window);
+// Thunk estático para conectar HWND -> T*
+template <typename T, bool Graphics>
+struct DispatchThunk : Dispatch<T, Graphics> {
+    static LRESULT CALLBACK thunk(HWND hwnd, UINT msg, WPARAM w, LPARAM l) {
+        T* window = nullptr;
+        if (msg == WM_NCCREATE) {
+            window = (T*)((LPCREATESTRUCT)l)->lpCreateParams;
+            window->handle = hwnd;
+            ::SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)window);
         } else {
-            window = Window(handle).object<T>();
+            window = (T*)::GetWindowLongPtr(hwnd, GWLP_USERDATA);
         }
-        if (window) return dispatch(window, msg, wparam, lparam);
-        return FALSE;
-    }
-    static INT_PTR dispatch(T* window, UINT msg, WPARAM wparam, LPARAM lparam) {
-        using namespace ui::native;
-        switch (msg) {
-        case WM_INITDIALOG: 
-            window->open(); 
-            return TRUE;
-        case WM_DESTROY: 
-            window->close();
-            if (window == app->mainWindow()) ::PostQuitMessage(0);
-            return TRUE;
-        case WM_COMMAND:
-            if (lparam) widget::Base::thunk_((HWND)lparam, HIWORD(wparam));
-            return TRUE;
-        case WM_DRAWITEM:
-            widget::Base::drawThunk_((DRAWITEMSTRUCT*)lparam);
-            return TRUE;
+
+        if (window) {
+            // Hook opcional del usuario
+            LRESULT res = 0;
+            if (window->msgHook(res, msg, w, l)) return res;
+            
+            return Dispatch<T, Graphics>::dispatch(window, msg, w, l);
         }
-        return FALSE;
+        return ::DefWindowProc(hwnd, msg, w, l);
     }
 };
 
+// Traits para Ventanas
 template <typename T>
-struct TraitsBase : Dispatch <T, !T::UsesGraphics> {
+struct TraitsBase : DispatchThunk <T, T::UsesGraphics> {
     enum {
-        classStyle = CS_DROPSHADOW * !!T::DropShadow,
-        styleEx    = WS_EX_DLGMODALFRAME * !!T::SysCaption,
-        style      = T::SysCaption ? WS_POPUP|WS_CAPTION|WS_SYSMENU : WS_POPUP
+        classStyle = CS_DBLCLKS | (T::DropShadow ? CS_DROPSHADOW : 0) | CS_HREDRAW | CS_VREDRAW,
+        styleEx    = T::SysCaption ? WS_EX_DLGMODALFRAME : 0,
+        style      = T::SysCaption ? (WS_POPUP | WS_CAPTION | WS_SYSMENU) : (WS_POPUP | WS_CLIPCHILDREN | WS_CLIPSIBLINGS)
     };
-    static const char* name() { return typeString<T>(); }
-    static bool create(const Window* parent, T* window) { return createWindow<TraitsBase>(parent, window); }
 };
-
-template <typename T> struct Traits : TraitsBase <T> {};
 
 template <typename T>
-struct AppTraits : Traits <T> {
-    enum { styleEx = 0, style = WS_POPUP|WS_SYSMENU|WS_MINIMIZEBOX }; // Simplified
-    static bool create(const Window* parent, T* window) { return createWindow<AppTraits>(parent, window); }
+struct Traits : TraitsBase <T> {
+    static const char* name() { return "KaliWindow"; }
+    static bool create(const Window* parent, T* window) { 
+        return createWindow<Traits>(parent, window); 
+    }
 };
 
+// Traits para Layers (Controles hijos)
 template <typename T>
 struct LayerTraits : Traits <T> {
-    enum { classStyle = 0, styleEx = WS_EX_CONTROLPARENT, style = WS_CHILD };
-    static bool create(const Window* parent, T* window) { return createWindow<LayerTraits>(parent, window); }
+    // Heredamos thunk pero cambiamos estilos
+    enum { 
+        classStyle = CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW,
+        styleEx    = WS_EX_CONTROLPARENT, 
+        style      = WS_CHILD | WS_VISIBLE | WS_CLIPCHILDREN | WS_CLIPSIBLINGS
+    };
+    static const char* name() { return "KaliLayer"; }
+    static bool create(const Window* parent, T* window) { 
+        return createWindow<LayerTraits>(parent, window); 
+    }
 };
 
 } // namespace details
 
+// Implementación de métodos de app
 template <typename T>
-int app::run(bool ItCouldBeOnlyOne) {
-    INITCOMMONCONTROLSEX icc = {sizeof(icc), ICC_WIN95_CLASSES};
-    ::InitCommonControlsEx(&icc);
-    
-    using kali::app;
-    app.alloc();
-    app->details_ = app->autorelease(new AppDetails);
-    app->details_->module_ = ::GetModuleHandle(0);
-    T* window = new T;
-    app->details_->mainWindow_ = window;
-
-    if (!details::AppTraits<T>::create(nullptr, window)) return 0;
-    app->autorelease(window);
-    
-    MSG msg;
-    while (::GetMessage(&msg, NULL, 0, 0)) {
-        if (!::IsDialogMessage(window->handle, &msg)) {
-            ::TranslateMessage(&msg); ::DispatchMessage(&msg);
-        }
-    }
-    app.release();
-    return (int)msg.wParam;
+bool app::createWindow(const Window* parent, T* window) { 
+    return details::Traits<T>::create(parent, window); 
 }
 
 template <typename T>
-bool app::createWindow(const Window* parent, T* window) { return details::Traits<T>::create(parent, window); }
+bool app::createLayer(const Window* parent, T* window) { 
+    return details::LayerTraits<T>::create(parent, window); 
+}
 
 template <typename T>
-bool app::createLayer(const Window* parent, T* window) { return details::LayerTraits<T>::create(parent, window); }
-
-template <typename T>
-bool app::loadLayer(const char* tag, const Window* parent, T* window) { 
-    return details::loadWindow<details::DispatchLoaded<T>>(tag, parent, window); 
+bool app::loadLayer(const char* tag, const Window* parent, T* window) {
+    // Stub para carga de recursos si fuera necesario
+    return createLayer(parent, window);
 }
 
 } // namespace kali

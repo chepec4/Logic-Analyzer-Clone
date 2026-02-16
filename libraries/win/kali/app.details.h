019a8e6bcdd8c4ee640066a2942491c565ea0e6d
chepec4 <chepec4@gmail.com>
2026-02-16 14:30:15 -0600

    Update app.details.h

--- libraries/win/kali/app.details.h
+++ libraries/win/kali/app.details.h
@@ -31,8 +31,8 @@ inline void    app::initGraphics() {
 
 namespace details {
 
-template <typename Traits>
-bool preinitWindow() {
+template <typename Traits, typename T>
+bool createWindow(const Window* parent, T* window) {
     WNDCLASSEX wcx = { sizeof(wcx) };
     wcx.style         = Traits::classStyle;
     wcx.hInstance     = app->module();
@@ -40,21 +40,17 @@ bool preinitWindow() {
     wcx.lpszClassName = Traits::name();
     wcx.hCursor       = ::LoadCursor(0, IDC_ARROW);
     wcx.hbrBackground = ::GetSysColorBrush(COLOR_3DFACE);
-    return !!::RegisterClassEx(&wcx);
-}
+    
+    ::RegisterClassEx(&wcx);
+    
+    // [C4 MASTER FIX] Cast explícito del error para evitar colisión con objetos temporales
+    if ((long)::GetLastError() != (long)ERROR_CLASS_ALREADY_EXISTS) { }
 
-template <typename Traits, typename T>
-bool createWindow(const Window* parent, T* window) {
-    Traits::initializer();
-    preinitWindow<Traits>();
-    if ((long)::GetLastError() != (long)ERROR_CLASS_ALREADY_EXISTS) {
-        // Log o manejo de error si es necesario
-    }
     HWND handle = ::CreateWindowEx((DWORD) Traits::styleEx,
         Traits::name(), Traits::name(), (DWORD) Traits::style,
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
         parent ? (HWND)parent->handle : 0, 0, app->module(), window);
-    if (handle && !::IsWindowVisible(handle)) ::ShowWindow(handle, SW_SHOWDEFAULT);
+    
     return !!handle;
 }
 
@@ -63,8 +59,9 @@ template <typename T, bool IsNative> struct Dispatch;
 template <typename T>
 struct Dispatch <T, false> {
     static void initializer() { app->initGraphics(); }
+    
     static LRESULT CALLBACK thunk(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam) {
-        T* window;
+        T* window = nullptr;
         if (msg == WM_NCCREATE && lparam) {
             window = (T*) ((CREATESTRUCT*) lparam)->lpCreateParams;
             window->handle = handle;
@@ -74,20 +71,21 @@ struct Dispatch <T, false> {
         }
         return window ? dispatch(window, msg, wparam, lparam) : ::DefWindowProc(handle, msg, wparam, lparam);
     }
+
     static LRESULT dispatch(T* window, UINT msg, WPARAM wparam, LPARAM lparam) {
         switch (msg) {
-        case WM_CREATE: return window->open() ? 0 : -1;
+        case WM_PAINT: {
+            PAINTSTRUCT ps; 
+            HDC dc = ::BeginPaint(window->handle, &ps);
+            Rect r(ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right-ps.rcPaint.left, ps.rcPaint.bottom-ps.rcPaint.top);
+            graphics::BufferedContext c(dc, r); 
+            window->draw(c);
+            ::EndPaint(window->handle, &ps); 
+            return 0;
+        }
         case WM_DESTROY: 
             window->close(); 
             return 0;
-        case WM_PAINT: {
-            PAINTSTRUCT ps; HDC dc = ::BeginPaint(window->handle, &ps);
-            if (ps.rcPaint.right > ps.rcPaint.left) {
-                Rect r(ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right-ps.rcPaint.left, ps.rcPaint.bottom-ps.rcPaint.top);
-                graphics::BufferedContext c(dc, r); window->draw(c);
-            }
-            ::EndPaint(window->handle, &ps); return 0;
-        }
         }
         return ::DefWindowProc(window->handle, msg, wparam, lparam);
     }
@@ -96,23 +94,18 @@ struct Dispatch <T, false> {
 template <typename T>
 struct TraitsBase : Dispatch <T, !T::UsesGraphics> {
     enum {
-        classStyle = CS_DROPSHADOW * !!T::DropShadow,
-        styleEx    = WS_EX_DLGMODALFRAME * !!T::SysCaption,
-        style      = T::SysCaption ? WS_POPUP|WS_CAPTION|WS_SYSMENU : WS_POPUP
+        classStyle = CS_DBLCLKS,
+        styleEx    = WS_EX_CONTROLPARENT,
+        style      = WS_CHILD | WS_VISIBLE
     };
-    static const char* name() { return typeString<T>(); }
+    static const char* name() { return "KaliWindow"; }
     static bool create(const Window* parent, T* window) { return createWindow<TraitsBase>(parent, window); }
 };
 
 template <typename T> struct Traits : TraitsBase <T> {};
+template <typename T> struct LayerTraits : Traits <T> {};
 
-template <typename T>
-struct LayerTraits : Traits <T> {
-    enum { classStyle = 0, styleEx = WS_EX_CONTROLPARENT, style = WS_CHILD };
-    static bool create(const Window* parent, T* window) { return createWindow<LayerTraits>(parent, window); }
-};
-
-} // namespace details
+} // ~ namespace details
 
 template <typename T>
 bool app::createWindow(const Window* parent, T* window) { return details::Traits<T>::create(parent, window); }
@@ -120,6 +113,6 @@ bool app::createWindow(const Window* parent, T* window) { return details::Traits
 template <typename T>
 bool app::createLayer(const Window* parent, T* window) { return details::LayerTraits<T>::create(parent, window); }
 
-} // namespace kali
+} // ~ namespace kali
 
-#endif // KALI_APP_DETAILS_INCLUDED
+#endif // ~ KALI_APP_DETAILS_INCLUDED

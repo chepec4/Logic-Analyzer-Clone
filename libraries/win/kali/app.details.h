4e4a2ae2ad93128e6a56fdfd6a1929ac270feec4
chepec4 <chepec4@gmail.com>
2026-02-16 14:39:58 -0600

    Update app.details.h

--- libraries/win/kali/app.details.h
+++ libraries/win/kali/app.details.h
@@ -5,7 +5,6 @@
 #include <commctrl.h>
 #include "kali/window.h"
 #include "kali/ui/native/widgets.h"
-#include "kali/resources.h"
 #include "kali/graphics.h"
 
 namespace kali {
@@ -19,7 +18,6 @@ struct AppDetails : ReleaseAny {
 
 inline Module* app::module()     const {return details_->module_;}
 inline Window* app::mainWindow() const {return details_->mainWindow_;}
-inline void    app::useThreads()       {}
 inline void    app::initGraphics() {
 #if defined KALI_GRAPHICS_INCLUDED
     if (!details_->graphics_) {
@@ -39,19 +37,13 @@ bool createWindow(const Window* parent, T* window) {
     wcx.lpfnWndProc   = Traits::thunk;
     wcx.lpszClassName = Traits::name();
     wcx.hCursor       = ::LoadCursor(0, IDC_ARROW);
-    wcx.hbrBackground = ::GetSysColorBrush(COLOR_3DFACE);
-    
+    wcx.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     ::RegisterClassEx(&wcx);
-    
-    // [C4 MASTER FIX] Cast explícito del error para evitar colisión con objetos temporales
-    if ((long)::GetLastError() != (long)ERROR_CLASS_ALREADY_EXISTS) { }
 
-    HWND handle = ::CreateWindowEx((DWORD) Traits::styleEx,
-        Traits::name(), Traits::name(), (DWORD) Traits::style,
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
+    HWND h = ::CreateWindowEx((DWORD) Traits::styleEx, Traits::name(), Traits::name(), 
+        (DWORD) Traits::style, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
         parent ? (HWND)parent->handle : 0, 0, app->module(), window);
-    
-    return !!handle;
+    return !!h;
 }
 
 template <typename T, bool IsNative> struct Dispatch;
@@ -59,45 +51,39 @@ template <typename T, bool IsNative> struct Dispatch;
 template <typename T>
 struct Dispatch <T, false> {
     static void initializer() { app->initGraphics(); }
-    
-    static LRESULT CALLBACK thunk(HWND handle, UINT msg, WPARAM wparam, LPARAM lparam) {
+    static LRESULT CALLBACK thunk(HWND handle, UINT msg, WPARAM w, LPARAM l) {
         T* window = nullptr;
-        if (msg == WM_NCCREATE && lparam) {
-            window = (T*) ((CREATESTRUCT*) lparam)->lpCreateParams;
+        if (msg == WM_NCCREATE && l) {
+            window = (T*) ((CREATESTRUCT*) l)->lpCreateParams;
             window->handle = handle;
-            window->object(window);
+            Window(handle).object(window);
         } else {
             window = Window(handle).object<T>();
         }
-        return window ? dispatch(window, msg, wparam, lparam) : ::DefWindowProc(handle, msg, wparam, lparam);
+        return window ? dispatch(window, msg, w, l) : ::DefWindowProc(handle, msg, w, l);
     }
-
-    static LRESULT dispatch(T* window, UINT msg, WPARAM wparam, LPARAM lparam) {
+    static LRESULT dispatch(T* window, UINT msg, WPARAM w, LPARAM l) {
         switch (msg) {
         case WM_PAINT: {
-            PAINTSTRUCT ps; 
-            HDC dc = ::BeginPaint(window->handle, &ps);
+            PAINTSTRUCT ps; HDC dc = ::BeginPaint(window->handle, &ps);
             Rect r(ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right-ps.rcPaint.left, ps.rcPaint.bottom-ps.rcPaint.top);
+            /**
+             * [C4 MASTER FIX] Tipo Completo BufferedContext.
+             * Soluciona: error: 'kali::graphics::BufferedContext c' has incomplete type
+             */
             graphics::BufferedContext c(dc, r); 
             window->draw(c);
-            ::EndPaint(window->handle, &ps); 
-            return 0;
+            ::EndPaint(window->handle, &ps); return 0;
         }
-        case WM_DESTROY: 
-            window->close(); 
-            return 0;
+        case WM_DESTROY: window->close(); return 0;
         }
-        return ::DefWindowProc(window->handle, msg, wparam, lparam);
+        return ::DefWindowProc(window->handle, msg, w, l);
     }
 };
 
 template <typename T>
 struct TraitsBase : Dispatch <T, !T::UsesGraphics> {
-    enum {
-        classStyle = CS_DBLCLKS,
-        styleEx    = WS_EX_CONTROLPARENT,
-        style      = WS_CHILD | WS_VISIBLE
-    };
+    enum { classStyle = CS_DBLCLKS, styleEx = WS_EX_CONTROLPARENT, style = WS_CHILD | WS_VISIBLE };
     static const char* name() { return "KaliWindow"; }
     static bool create(const Window* parent, T* window) { return createWindow<TraitsBase>(parent, window); }
 };
@@ -105,7 +91,7 @@ struct TraitsBase : Dispatch <T, !T::UsesGraphics> {
 template <typename T> struct Traits : TraitsBase <T> {};
 template <typename T> struct LayerTraits : Traits <T> {};
 
-} // ~ namespace details
+} // namespace details
 
 template <typename T>
 bool app::createWindow(const Window* parent, T* window) { return details::Traits<T>::create(parent, window); }
@@ -113,6 +99,5 @@ bool app::createWindow(const Window* parent, T* window) { return details::Traits
 template <typename T>
 bool app::createLayer(const Window* parent, T* window) { return details::LayerTraits<T>::create(parent, window); }
 
-} // ~ namespace kali
-
-#endif // ~ KALI_APP_DETAILS_INCLUDED
+} // namespace kali
+#endif

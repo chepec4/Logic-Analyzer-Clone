332f7dcdad17eb2f2cdd57286b6ae45fdffdbb35
chepec4 <chepec4@gmail.com>
2026-02-16 22:55:04 -0600

    Update native.h

--- libraries/kali/ui/native.h
+++ libraries/kali/ui/native.h
@@ -7,9 +7,10 @@ namespace kali {
 namespace ui {
 namespace native {
 
-// [C4 MASTER FIX] Declaraciones adelantadas para romper la dependencia circular con widgets.h
+// [C4 MASTER SYNC] Declaraciones adelantadas obligatorias para GCC 12
 namespace widget {
     struct Interface;
+    struct Base;
     struct Button;
     struct Combo;
     struct Text;
@@ -19,23 +20,23 @@ namespace widget {
     struct ColorWell;
     struct Stepper;
     struct Fader;
+    struct LayerTabs; // Requerido por sa.editor.h
 }
 
-// 1. TYPEDEFS (Punteros Inteligentes)
+// 1. TYPEDEFS DE PUNTEROS (Resolución de visibilidad)
 typedef Ptr<widget::Interface> AnyWidget;
 typedef Ptr<widget::Button>    Button;
 typedef Ptr<widget::Combo>     Combo;
 typedef Ptr<widget::Text>      Text;
-typedef Ptr<widget::Text>      TextRight;
-typedef Ptr<widget::Text>      Label;
 typedef Ptr<widget::Edit>      Edit;
 typedef Ptr<widget::Toggle>    Toggle;
 typedef Ptr<widget::Toolbar>   Toolbar;
 typedef Ptr<widget::ColorWell> ColorWell;
 typedef Ptr<widget::Stepper>   Stepper;
 typedef Ptr<widget::Fader>     Fader;
+typedef Ptr<widget::LayerTabs> LayerTabsPtr; // Fix: LayerTabsPtr does not name a type
 
-// 2. WINDOW BASE
+// 2. CLASES BASE DE VENTANA
 struct WindowBase : Window {
     enum { UsesGraphics = 1, DropShadow = 0, SysCaption = 0 };
     virtual ~WindowBase() {}
@@ -46,7 +47,6 @@ struct WindowBase : Window {
         this->position(r.x + (r.w - s.w) / 2, r.y + (r.h - s.h) / 2);
     }
     
-    // Firma virtual requerida para el dispatcher
     virtual void draw(graphics::BufferedContext&) {}
 };
 
@@ -59,7 +59,7 @@ struct LayerBase : WindowBase {
 } // namespace ui
 } // namespace kali
 
-// Incluimos la implementación de plataforma al final para que vea los typedefs
+// Incluimos la implementación de plataforma para poblar los namespaces anteriores
 #include "kali/ui/native/widgets.h"
 
 #endif
